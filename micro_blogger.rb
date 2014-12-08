39d93334b7fb590f20bb6265599045e1a01a1eb1
Vidul Parthasarathy <viparthasarathy@gmail.com>
2014-12-08 14:29:37 -0800

    final

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -1,4 +1,7 @@
 require 'jumpstart_auth'
+require 'bitly'
+
+Bitly.use_api_version_3
 
 class MicroBlogger
   attr_reader :client
@@ -6,6 +9,12 @@ class MicroBlogger
   def initialize
     puts "Initializing..."
     @client = JumpstartAuth.twitter
+  end
+
+  def shorten(original_url)
+  	puts "Shortening this URL: #{original_url}"
+  	bitly = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
+  	bitly.shorten(original_url).short_url
   end
 
   def tweet(message)
@@ -17,17 +26,37 @@ class MicroBlogger
   	end
   end
 
+  def followers_list
+  	screen_names = []
+  	@client.followers.each { |follower| screen_names << @client.user(follower).screen_name }
+    screen_names
+  end
+
+  def spam_my_followers(message)
+  	followers_list.each { |follower| dm(follower, message) }
+  end
+
   def dm(target, message)
   	puts "Trying to send #{target} this direct message:"
   	puts message
   	message = "d @#{target} #{message}"
-  	screen_names = @client.followers.collect {|follower| @client.user(follower).screen_name }
-  	if screen_names.includes? target
+  	if followers_list.include? target
   	  tweet(message)
   	else
   	  puts "You can only direct message people who follow you."
   	end
   end
+
+   
+  def everyones_last_tweet
+  	friends = @client.friends.sort_by { |friend| @client.user(friend).screen_name.downcase }
+  	friends.each do |friend|
+  	  timestamp = @client.user(friend).created_at
+      puts "#{@client.user(friend).screen_name}, #{timestamp.strftime('%A, %b %d')}, says:"
+      puts @client.user(friend).status.text
+  	end
+  end
+
 
   def run
   	puts "Welcome to the JSL Twitter Client!"
@@ -42,6 +71,10 @@ class MicroBlogger
   		when 'q' then puts "Goodbye!"
   		when 't' then tweet(parts[1..-1].join(" "))
   		when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+  		when 'spam' then spam_my_followers(parts[1..-1].join(" "))
+  		when 'elt' then everyones_last_tweet
+  		when 's' then shorten(parts[1..-1].join)
+  		when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
   		else
   		  puts "Sorry, I don't know how to #{command}"
   		end
